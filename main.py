1fd67c66667a7fb4c96056ebf1557d0114b4ff78
Morgana Rodrigues <morganadiasrodrigues@outlook.com>
2024-10-24 11:11:10 -0300

    main com linhas para debug

--- main.py
+++ main.py
@@ -15,18 +15,25 @@ def create_board(n):
 
 # Verifica se eu nao vou apontar pra ninguem da minha quadrilha
 def is_safe(board, row, col, quadrilha):
+    print(f"Verificando a safety para quadrilha {quadrilha}")
+    print(f"Linha e coluna atual: {(row, col)}")
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
     for dr, dc in directions:
+        print(f"Direções: {(dr, dc)}")
         r, c = row + dr, col + dc
+        print(f"Linha e coluna do for: {(r, c)}")
         blocked = False
         while 0 <= r < n and 0 <= c < n:
             cell = board[r][c]
             if cell == ".":
+                print("Cell nesta linha e coluna está livre!")
                 r += dr
                 c += dc
+                print(f"Linha e coluna do laço while: {(r, c)}")
                 continue
             elif cell == quadrilha:
+                print("Cell nesta linha e coluna tem a mesma quadrilha!")
                 if not blocked:
                     return False
                 else:
@@ -35,23 +42,31 @@ def is_safe(board, row, col, quadrilha):
                 blocked = True
             r += dr
             c += dc
+            print(f"Linha e coluna do fim do laço while: {(r, c)}")
     return True
 
 
 def sees_at_least_two_enemies(board, row, col, quadrilha, n):
+    print("Verificando se tem pelo menos 2 inimigos...")
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     count = 0
     for dr, dc in directions:
+        print(f"Direções: {(dr, dc)}")
         r, c = row + dr, col + dc
+        print(f"Linha e coluna do for: {(r, c)}")
         while 0 <= r < n and 0 <= c < n:
             cell = board[r][c]
             if cell == ".":
+                print("Cell nesta linha e coluna está livre!")
                 r += dr
                 c += dc
+                print(f"Linha e coluna do laço while: {(r, c)}")
                 if count >= 2:
+                    print("Temos ao menos dois inimigos!")
                     return True
                 continue
             elif cell != quadrilha:
+                print("Cell nesta linha e coluna tem um inimigo!")
                 count += 1
                 break
             else:
@@ -63,13 +78,17 @@ def sees_at_least_two_enemies(board, row, col, quadrilha, n):
 
 def validate_board(board):
     if hash_board(board) not in solutions_set:
+        print("Hash deste board não existe!")
         for row in range(n):
             for col in range(n):
                 cell = board[row][col]
                 if cell != ".":
+                    print(f"Cell atual tem uma quadrilha: {cell}, posição: {(row, col)}")
                     if not sees_at_least_two_enemies(board, row, col, cell, n):
+                        print("Não há inimigos o suficiente!")
                         return
         solutions_set.add(hash_board(board))
+        print("Hash deste board foi adicionado!")
         transforms(board, n)
 
 
@@ -81,12 +100,11 @@ def print_board(board, solution):
 
 
 def transforms(board, n):
-
+    print("Iniciando rotações...")
     def rotate(board):
         return [[board[n - j - 1][i] for j in range(n)] for i in range(n)]
 
     def mirror_matrix(board):
-        # Mirror each row of the matrix by reversing it
         mirrored_board = [row[::-1] for row in board]
         return mirrored_board
 
@@ -103,7 +121,9 @@ def transforms(board, n):
 
 
 def backtrack(board, b_restantes, c_restantes, total_positions, index):
+    print("starting backtrack...")
     if b_restantes == 0 and c_restantes == 0:
+        print("Não há quadrilhas para posicionar - validando board...")
         validate_board(board)
         return
 
@@ -111,8 +131,9 @@ def backtrack(board, b_restantes, c_restantes, total_positions, index):
         return
 
     row, col = divmod(index, n)
-
+    print(f"Current row and col: {(row, col)}")
     if board[row][col] == ".":
+        print("Board has a free position!")
         # Tenta colocar um 'B'
         if b_restantes > 0 and is_safe(board, row, col, "B"):
             board[row][col] = "B"
